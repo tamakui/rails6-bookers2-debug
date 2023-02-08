f264638ba80888e7d43cbef434a6030f7655a85a
EC2 Default User <ec2-user@ip-172-31-44-91.ap-northeast-1.compute.internal>
2023-02-08 08:53:51 +0000

    [Finish?]6

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,6 +13,14 @@ import "bootstrap"
 import '@fortawesome/fontawesome-free/js/all';
 import "../stylesheets/application" 
 
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+
+require('jquery')
+require("test.js")
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
